31858c0ea3b964b5222138381f2a9e5aad49160e
wbolen <47831381+wbolen@users.noreply.github.com>
2020-11-09 15:45:43 -0500

    Create Auction.ts

---
+++ src/app/models/Auction.ts
@@ -0,0 +1,11 @@
+import {Items} from './Items';
+
+export class Auction{
+  company: string;
+  auctioneer: string;
+  contact: string;
+  address: string;
+  time: string;
+  date: string;
+  items: Items[];
+}
