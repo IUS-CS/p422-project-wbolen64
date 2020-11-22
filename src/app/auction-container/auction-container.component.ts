3c599d39547c37d22152f6df4bba222f6b2761b1
wbolen <47831381+wbolen@users.noreply.github.com>
2020-11-22 14:33:48 -0500

    Update auction-container.component.ts

--- src/app/auction-container/auction-container.component.ts
+++ src/app/auction-container/auction-container.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import {AUCTIONS} from '../data/auctions';
-import {Auction} from '../models/Auction';
+import { Component, OnInit} from '@angular/core';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
+import {Auction, AuctionDataService} from '../auction-data.service';
+import {Observable} from 'rxjs';
+import {switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-auction-container',
@@ -8,18 +10,56 @@ import {Auction} from '../models/Auction';
   styleUrls: ['./auction-container.component.css']
 })
 export class AuctionContainerComponent implements OnInit {
-auctions = AUCTIONS;
-selectedAuction: Auction;
-model = 1;
-  constructor() { }
+  selectedAuctionDate: string;
+  selectedAuction: Observable<Auction>;
+
+  constructor(
+    private auctionDataService: AuctionDataService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
+
   ngOnInit(): void {
+    this.selectedAuction = this.route.paramMap.pipe(
+      switchMap((params: ParamMap): Observable<Auction> => {
+        return this.auctionDataService.getAuction(params.get('class'));
+      })
+    );
   }
-  onSelect(auction: Auction ) {
-    if (this.selectedAuction && this.selectedAuction.date === auction.date){
-      this.selectedAuction = null;
-      return;
+
+  public selectCourse(name: string): void {
+    this.selectedAuction = this.auctionDataService.getAuction(name);
+    if (!this.selectedAuction) {
+      this.router.navigateByUrl('/notfound');
     }
-    this.selectedAuction = auction;
-    console.log(auction.date);
   }
 }
+
+/*
+import { Component, OnInit} from '@angular/core';
+import {AUCTIONS} from '../data/auctions';
+import {Auction} from '../models/Auction';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-auction-container',
+  templateUrl: './auction-container.component.html',
+  styleUrls: ['./auction-container.component.css']
+})
+export class AuctionContainerComponent implements OnInit {
+     auctions = AUCTIONS;
+   selectedAuction: Auction;
+   model = 1;
+     constructor() { }
+     ngOnInit(): void {
+     }
+     onSelect(auction: Auction ) {
+       if (this.selectedAuction && this.selectedAuction.date === auction.date){
+         this.selectedAuction = null;
+         return;
+       }
+       this.selectedAuction = auction;
+       console.log(auction.date);
+     }
+}
+*/
